72122bf29c94abc12839e12e055dadabf817c57f
brun0-matheus <brunomatheus2607@gmail.com>
2021-11-11 13:31:02 -0300

    Fazendo APIs das edições de valor hora e horario padrão (sem testes)

--- src/api/estacionamento_others_api.py
+++ src/api/estacionamento_others_api.py
@@ -1,4 +1,7 @@
 import logging
+import datetime
+from typing import Optional
+from decimal import Decimal
 
 import flask
 from ariadne import convert_kwargs_to_snake_case
@@ -20,7 +23,13 @@ class EstacionamentoOthersApi(BaseApi):
         self.repo = repo
 
         queries = {}
-        mutations = {'atualizarQtdVagaLivre': self.atualizar_qtd_vaga_livre_resolver}
+        mutations = {
+            'atualizarQtdVagaLivre': self.atualizar_qtd_vaga_livre_resolver,
+            'editEstacioHorarioPadrao': self.edit_horario_padrao_resolver,
+            'editEstacioValorHora': self.edit_valor_hora_resolver,
+            'deleteEstacioValorHora': self.delete_valor_hora_resolver,
+            'deleteEstacioHorarioPadrao': self.delete_horario_padrao_resolver
+        }
 
         super().__init__(queries, mutations)
 
@@ -44,3 +53,80 @@ class EstacionamentoOthersApi(BaseApi):
                 'success': False,
                 'error': error
             }
+
+    @convert_kwargs_to_snake_case
+    def edit_horario_padrao_resolver(self, _, info, dia: str, hora_abre: datetime.time, hora_fecha: datetime.time,
+                                     estacio_id: Optional[str] = None):
+        sess: Session = flask.g.session
+        user_sess = self.get_user_session(sess, self.cached, info)
+
+        try:
+            success, error_or_hora = self.repo.edit_horario_padrao(user_sess, sess, dia, hora_abre, hora_fecha, estacio_id)
+        except Exception as ex:
+            logging.getLogger(__name__).error('Error on edit_horario_padrao_resolver', exc_info=ex)
+            success, error_or_hora = False, self.ERRO_DESCONHECIDO
+
+        if success:
+            return {
+                'success': True,
+                'horario_padrao': error_or_hora
+            }
+        else:
+            return {
+                'success': False,
+                'error': error_or_hora
+            }
+
+    @convert_kwargs_to_snake_case
+    def edit_valor_hora_resolver(self, _, info, veiculo_id: str, valor: Decimal, estacio_id: Optional[str] = None):
+        sess: Session = flask.g.session
+        user_sess = self.get_user_session(sess, self.cached, info)
+
+        try:
+            success, error_or_valor = self.repo.edit_valor_hora(user_sess, sess, veiculo_id, valor, estacio_id)
+        except Exception as ex:
+            logging.getLogger(__name__).error('Error on edit_valor_hora_resolver', exc_info=ex)
+            success, error_or_valor = False, self.ERRO_DESCONHECIDO
+
+        if success:
+            return {
+                'success': True,
+                'valor_hora': error_or_valor
+            }
+        else:
+            return {
+                'success': False,
+                'error': error_or_valor
+            }
+
+    @convert_kwargs_to_snake_case
+    def delete_horario_padrao_resolver(self, _, info, dia: str, estacio_id: Optional[str] = None):
+        sess: Session = flask.g.session
+        user_sess = self.get_user_session(sess, self.cached, info)
+
+        try:
+            success, error = self.repo.edit_horario_padrao(user_sess, sess, dia, None, None, estacio_id)
+        except Exception as ex:
+            logging.getLogger(__name__).error('Error on delete_horario_padrao_resolver', exc_info=ex)
+            success, error = False, self.ERRO_DESCONHECIDO
+
+        if success:
+            return {'success': True}
+        else:
+            return {'success': False, 'error': error}
+
+    @convert_kwargs_to_snake_case
+    def delete_valor_hora_resolver(self, _, info, veiculo_id: str, estacio_id: Optional[str] = None):
+        sess: Session = flask.g.session
+        user_sess = self.get_user_session(sess, self.cached, info)
+
+        try:
+            success, error = self.repo.delete_valor_hora(user_sess, sess, veiculo_id, estacio_id)
+        except Exception as ex:
+            logging.getLogger(__name__).error('Error on delete_valor_hora_resolver', exc_info=ex)
+            success, error = False, self.ERRO_DESCONHECIDO
+
+        if success:
+            return {'success': True}
+        else:
+            return {'success': False, 'error': error}
